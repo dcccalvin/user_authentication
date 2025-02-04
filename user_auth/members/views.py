ac0a195df988980a4cb7e138ce88d5cd17c3d44b
David calvin <chumacalvin8@gmail.com>
2025-02-04 17:46:57 +0300

    Already linked in user cannot visit registration or login unless logged out

--- user_auth/members/views.py
+++ user_auth/members/views.py
@@ -9,37 +9,42 @@ from django.contrib.auth.decorators import login_required
 @login_required(login_url='login')
 def home(request):
     return render(request, 'others/home.html', {})
-
+        
 def register(request):
     form = CreateUserForm()
     context = {'form': form}
-
-    if request.method == 'POST':
-        form = CreateUserForm(request.POST)  
-        if form.is_valid():
-            form.save()
-            user=form.cleaned_data.get('username')
-            messages.success(request,"Account was created for " + user)
-            
-            return redirect('login')  # ✅ Redirect after successful registration
-        else:
-            messages.error(request, "There was an error with your registration. Please check the details below.")
+    if request.user.is_authenticated:
+        return redirect('home')
     else:
-        form = CreateUserForm()
-    return render(request, 'user/register.html', context)  # ✅ Added return statement for GET requests
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)  
+            if form.is_valid():
+                form.save()
+                user=form.cleaned_data.get('username')
+                messages.success(request,"Account was created for " + user)
+                
+                return redirect('login')  # ✅ Redirect after successful registration
+            else:
+                messages.error(request, "There was an error with your registration. Please check the details below.")
+        else:
+            form = CreateUserForm()
+        return render(request, 'user/register.html', context)  # ✅ Added return statement for GET requests
 
 def login_view(request):  
-    if request.method== 'POST':
-        username=request.POST.get('username')
-        password=request.POST.get('password')
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method== 'POST':
+            username=request.POST.get('username')
+            password=request.POST.get('password')
 
-        user = authenticate(request, username= username,password=password)
-        if user is not None:
-            login(request,user)
-            return redirect('home')
-        else:
-            messages.info(request,"username or password is incorrect")
-    return render(request, 'user/login.html', {})
+            user = authenticate(request, username= username,password=password)
+            if user is not None:
+                login(request,user)
+                return redirect('home')
+            else:
+                messages.info(request,"username or password is incorrect")
+        return render(request, 'user/login.html', {})
 
 def logout_view(request):
     logout(request)
